a3a44d143e30755be96b9c616eb05b5d5906475f
22dcolli <144738226+22dcolli@users.noreply.github.com>
2024-11-19 16:10:21 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -18,15 +18,13 @@ assert(JSON.stringify(dijkstra(classGraph, 2)) == JSON.stringify([9,11,0,13,8,11
 assert(JSON.stringify(dijkstra(classGraph, 4)) == JSON.stringify([18,20,9,7,0,3,1,6]));
 //end of example section
 
-/*
-let loopTest = [
-  [0, 1, 0, 0],
-  [0, 0, 2, 3],
+
+let emptyTest = [
   [0, 0, 0, 0],
-  [1, 0, 0, 0]
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0]
 ];
+assert(JSON.stringify(dijkstra(loopTest,0)) == JSON.stringify([0,Infinity,Infinity,Infinity]));
+assert(JSON.stringify(dijkstra(loopTest,2)) == JSON.stringify([Infinity,Infinity,0,Infinity]));
 
-
-assert(JSON.stringify(dijkstra(loopTest,0)) == JSON.stringify([0,1,3,4]));
-assert(JSON.stringify(dijkstra(loopTest,3)) == JSON.stringify([1,Infinity,Infinity,4]));
-*/
